18dff41483b8f3cf083ab220f4100944b49a5124
soynikovd <161854856+soynikovd@users.noreply.github.com>
2024-10-31 18:07:18 +0200

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+//	int health = 100;
+//	int stamina = 100;
+//	int money = 0;
+//
+//	public static void walk() {
+//		System.out.println("Character go to shop");
+//
+//	}
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
